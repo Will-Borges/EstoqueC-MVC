b076fa1d428e60bc2a005aa946a4e6f369f8bb66
William Borges <36491847+Will-Borges@users.noreply.github.com>
2021-01-13 13:37:06 -0300

    Entrada de Produtos

--- SistemaEstoque/Controllers/EstoqueController.cs
+++ SistemaEstoque/Controllers/EstoqueController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SistemaEstoque.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -10,12 +11,14 @@ namespace SistemaEstoque.Controllers
     {
         // GET: Estoque
         public ActionResult Index()
-        {
+        {           
             return View();
         }
 
         public ActionResult EntradaEstoque()
         {
+            DBContext db = new DBContext();
+            ViewBag.ProdutosDescricao = new SelectList(db.Produtos, "id","descricao");
             return View();
         }
     }
